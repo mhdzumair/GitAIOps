db44bb9c70ade3e776844908c2a355abe2ad5829
mhdzumair <mhdzumair@gmail.com>
2023-05-21 10:01:45 +0530

    update auth header name

--- utils/api_config.py
+++ utils/api_config.py
@@ -5,11 +5,11 @@ def get_api_token(service, request):
     """
     Return the API token for the given service.
     """
-    if token := request.headers.get("PRIVATE-TOKEN"):
+    if token := request.headers.get("Authorization"):
         api_token = token.lstrip("Bearer ")
     else:
         api_token = os.environ.get(f"{service}_TOKEN")
 
     if not api_token:
-        raise Exception(f"Missing {service}_TOKEN")
+        raise RuntimeError(f"Missing {service}_TOKEN")
     return api_token
