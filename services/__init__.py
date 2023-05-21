e79949806efff9129458b3bc1fe9d4ab32434922
mhdzumair <mhdzumair@gmail.com>
2023-05-21 08:41:02 +0530

    update endpoint prefixes

--- services/__init__.py
+++ services/__init__.py
@@ -8,9 +8,9 @@ SERVICE_NAME = os.getenv("GITAIOPS_SERVICE_NAME", "gitlab")
 if SERVICE_NAME == "gitlab":
     from services.gitlab import router as gitlab_router
 
-    services_router.include_router(gitlab_router, tags=["GitLab"])
+    services_router.include_router(gitlab_router, prefix="/gitlab", tags=["GitLab"])
 
 elif SERVICE_NAME == "github":
     from services.github import router as github_router
 
-    services_router.include_router(github_router, tags=["GitHub"])
+    services_router.include_router(github_router, prefix="/github", tags=["GitHub"])
